55a50cc4b3ce89ea435d2486e15b4c38d882ccd5
jguzman-bds <115578368+jguzman-bds@users.noreply.github.com>
2022-10-24 14:20:32 +0200

    Add files via upload

---
+++ FirstPage.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+import altair as alt
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.title('Welcome')
+st.write('Welcome to the Streamlit dashboard.')
